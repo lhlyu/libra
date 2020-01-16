70c9498dc82f7a399951675db5a887bba35cf575
lhlyu <2438211370@qq.com>
2020-01-16 12:27:07 +0800

    seed

--- module/log.go
+++ module/log.go
@@ -2,10 +2,10 @@ package module
 
 import (
 	"github.com/lhlyu/libra/common"
-    "github.com/lhlyu/yutil"
-    "github.com/sirupsen/logrus"
+	"github.com/lhlyu/yutil"
+	"github.com/sirupsen/logrus"
 	"os"
-    "path"
+	"path"
 )
 
 type lg struct {
@@ -19,6 +19,7 @@ func (lg) SetUp() {
 	common.L = NewEntry()
 }
 
+// 日志模块
 var LgModule = lg{}
 
 func NewEntry() *logrus.Entry {
@@ -26,11 +27,11 @@ func NewEntry() *logrus.Entry {
 	out := common.Cfg.GetString("log.out")
 	level := common.Cfg.GetString("log.level")
 	if out != "" {
-	    dir := path.Dir(out)
-	    exists := yutil.FileIsExists(dir)
-	    if !exists{
-	        os.MkdirAll(dir,0666)
-        }
+		dir := path.Dir(out)
+		exists := yutil.FileIsExists(dir)
+		if !exists {
+			os.MkdirAll(dir, 0666)
+		}
 		f, err := os.OpenFile(out, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 		if err != nil {
 			panic(err)
@@ -50,18 +51,3 @@ func NewEntry() *logrus.Entry {
 	entry := logrus.NewEntry(lr)
 	return entry
 }
-
-/**
-一个简易的日志格式:
-{
-  "CREATEDAT": "2019-12-09 12:05:56",                                       // 创建时间
-  "FNAME": "github.com/lhlyu/libra/middleware.Log.func1",                     // 调用函数
-  "LAYER": "middleware",                                                    // 包/层级
-  "LEVEL": "debug",                                                         // 等级
-  "PARAM": [                                                                // 信息
-    "[1] 127.0.0.1 ▶ GET:/api/articles"
-  ],
-  "POSITION": "/iyu/middleware/log.go:15",                                  // 调用定位
-  "TRACEID": "6f0b160d490a71"                                               // 本次请求唯一ID，用于日志追踪
-}
-*/
